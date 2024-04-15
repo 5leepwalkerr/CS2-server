06b2119be9f442597800bf5399027a9a0714664f
s1eepwalkerr <cvb.cvb.2017.cvb@gmail.com>
2024-04-16 01:43:08 +0300

    changes config

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ $Website_Subfolder = '';
 
 // Place here your domain if steam authorize not working properly,
 // if its working good then leave it empty.
-$Website_Domain = '';
+$Website_Domain = 'https://cs2-server-production.up.railway.app/';
 
 // Enable this if you want categories else it will display all weapons.
 $Website_UseCategories = true;
