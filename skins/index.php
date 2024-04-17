6091dbc9164f626ec7f8595bb72fdd505b1c9870
s1eepwalkerr <cvb.cvb.2017.cvb@gmail.com>
2024-04-17 19:05:44 +0300

    test

--- skins/index.php
+++ skins/index.php
@@ -287,7 +287,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="../src/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../src/logo.jpg" type="image/x-icon">
     <link rel="stylesheet" href="../css/main.css" type="text/css">
     <link rel="stylesheet" href="../css/skins.css" type="text/css">
     <script src="script.js" defer></script>
@@ -828,9 +828,7 @@ if(!isset($_POST['weapon'])) {
             <img src="<?= $UserInfo->avatarfull ?>" alt="name">
             <p><?= str_replace('{{name}}', "<strong>$UserInfo->personaname</strong>", $translations->skins->footer->signedin); ?></p>
         </a>
-        <div class="credit">
-            <p>This website created by LielXD</p>
-        </div>
+
         <div class="actions">
             <div class="settings">
                 <svg viewBox="0 0 48 48" data-action="toggle_menu"><path d="M0 0h48v48H0z" fill="none"/><path d="M38.86 25.95c.08-.64.14-1.29.14-1.95s-.06-1.31-.14-1.95l4.23-3.31c.38-.3.49-.84.24-1.28l-4-6.93c-.25-.43-.77-.61-1.22-.43l-4.98 2.01c-1.03-.79-2.16-1.46-3.38-1.97L29 4.84c-.09-.47-.5-.84-1-.84h-8c-.5 0-.91.37-.99.84l-.75 5.3c-1.22.51-2.35 1.17-3.38 1.97L9.9 10.1c-.45-.17-.97 0-1.22.43l-4 6.93c-.25.43-.14.97.24 1.28l4.22 3.31C9.06 22.69 9 23.34 9 24s.06 1.31.14 1.95l-4.22 3.31c-.38.3-.49.84-.24 1.28l4 6.93c.25.43.77.61 1.22.43l4.98-2.01c1.03.79 2.16 1.46 3.38 1.97l.75 5.3c.08.47.49.84.99.84h8c.5 0 .91-.37.99-.84l.75-5.3c1.22-.51 2.35-1.17 3.38-1.97l4.98 2.01c.45.17.97 0 1.22-.43l4-6.93c.25-.43.14-.97-.24-1.28l-4.22-3.31zM24 31c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/></svg>
